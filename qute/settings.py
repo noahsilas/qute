93f2c4bbbc8d4c773c3fc8f0ac66d45a10ee6925
Noah Silas <noah@silas.cc>
2012-04-01 10:48:15 -0700

    Add bootstrap / boilerplate

--- qute/settings.py
+++ qute/settings.py
@@ -1,4 +1,5 @@
 # Django settings for qute project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -70,6 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -109,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
