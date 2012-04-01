1d5cc9b873f7701a08169f175e7a4a0e8d7812c1
Noah Silas <noah@silas.cc>
2012-03-31 23:50:35 -0700

    Move quote scoring logic into manager/queryset

--- qdb/models.py
+++ qdb/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from hideable.models import Hideable, HideableManager
 
+class QuoteManager(HideableManager):
+    class QuerySet(HideableManager.QuerySet):
+        def with_score(self):
+            "Annotates this QS with the voting score"
+            return self.annotate(score=models.Sum('vote__score'))
+
+    def get_query_set(self):
+        return QuoteManager.QuerySet(self.model, using=self._db)
+
+    def with_score(self):
+        return self.get_query_set().with_score()
+
+
 class Quote(Hideable, models.Model):
     body = models.TextField()
     ip = models.GenericIPAddressField()
 
-    def score(self):
-        return self.vote_set.aggregate(models.Sum('score'))['score']
+    objects = QuoteManager()
 
 
 class Vote(Hideable, models.Model):
